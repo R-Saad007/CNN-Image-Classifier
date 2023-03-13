5114617c752cbcd16928e9d7070fc46fc3c69de7
Rana Saad Imran <22100018@lums.edu.pk>
2023-03-13 13:11:04 +0500

    Update test.py

--- test.py
+++ test.py
@@ -3,23 +3,28 @@ from normalize_load_data import test_dataloader
 from cnn import *
 
 # Test function
-def test():
+def test(save_flag):
     correct = 0
+    accuracy = 0
     total = 0
     with torch.no_grad():
         for data in test_dataloader:
             images, labels = data
             images = images.to(device)
             labels = labels.to(device)
-            outputs = CNN_net(images).to(device)
+            outputs = CNN_net(images)
+            outputs = outputs.to(device)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
     # accuracy
+    accuracy = (correct / total) * 100
     print('Finished Testing')
-    print('Accuracy of the network on the 10000 test images: %.2f %%' % (100 * correct / total))
-    # to save the CNN model
-    PATH = './cifar_net.pth'
-    torch.save(CNN_net.state_dict(), PATH)
-    print('Model Saved')
+    print('Accuracy on the 10000 test images: %.2f %%' % accuracy)
+    if save_flag:
+        # to save the CNN model
+        PATH = './cifar_net.pth'
+        torch.save(CNN_net.state_dict(), PATH)
+        print('Model Saved')
+    return accuracy
